d7a1116497661eb17a9e14c67103f7b025aeaa83
bimanmandal <biman29@gmail.com>
2022-01-07 09:33:09 +0530

    added the code changes for chapter 2

--- code/chap02/scala/src/main/scala/org/data/algorithms/spark/ch02/DNABaseCountVER3.scala
+++ code/chap02/scala/src/main/scala/org/data/algorithms/spark/ch02/DNABaseCountVER3.scala
@@ -1,46 +1,35 @@
 package org.data.algorithms.spark.ch02
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.storage.StorageLevel
+import org.data.algorithms.spark.ch02.DNABaseCountFastq.{drop3Records, processFASTQPartition}
 
 import scala.collection.mutable
 
 /**
  *-----------------------------------------------------
  * Version-3
- * This is a DNA-Base-Count in Spark using FASQT input data.
+ * This is a DNA-Base-Count in Spark Using Scala.
  * The goal is to show how "DNA-Base-Count" works.
  *------------------------------------------------------
  * Input Parameters:
- *    argv[1]: String, input path for FASTQ data
+ *    String, input path
  *-------------------------------------------------------
  *
  * @author Biman Mandal
  *-------------------------------------------------------
  */
+object DNABaseCountVER3 {
 
-object DNABaseCountFastq {
-
-  def drop3Records(rec: String) : Boolean = {
-    val firstChar = rec.charAt(0)
-    // drop Line 1
-    if (firstChar == '@')
-      return false
-    // drop Line 3
-    if (firstChar == '+')
-      return false
-
-    // drop Line 4 & keep Line 2
-    val nonDnaLetters = Set.from("-+*/<>=.:@?;0123456789bde".toCharArray)
-    if(rec.toCharArray.exists(nonDnaLetters.contains)){
-      // drop Line 4
-      println("rec=", rec)
-      false
-    } else
-      // found Line 2: DNA Sequence
-      true
-  }
-
+  /**
+   *-----------------------------------
+   *
+   * we get an iterator: which represents
+   * a single partition of source RDD
+   *
+   * This function creates a hash map of DNA Letters
+   * "z" denotes the total number of FASTA Records for a single partition
+   *
+   */
   def processFASTQPartition(iterator: Iterator[String]): Iterator[(Char, Int)] = {
     // create an empty dictionary
     val hashMap = new mutable.HashMap[Char, Int]()
@@ -55,24 +44,10 @@ object DNABaseCountFastq {
     hashMap.iterator
   }
 
-  /**
-   *-----------------------------------
-   * rec: a single record of FASTQ file
-   *
-   * drop Lines 1, 3, and 4
-   * keep only Line2, which is a DNA sequence
-   *-----------------------------------
-   */
-
   def main(args: Array[String]): Unit = {
-    if (args.length != 1) {
-      System.err.println("Input path is missing in the command line argument")
-      System.exit(-1)
-    }
-
     val spark =
       SparkSession.builder()
-        .appName("DNABaseCountFastq")
+        .appName("DNABaseCountVER3")
         .master("local[*]")
         .getOrCreate()
 
@@ -84,17 +59,10 @@ object DNABaseCountFastq {
         .map(_.toLowerCase)
     println(s"recordsRdd : ${recordsRdd.collect().mkString("Array(", ", ", ")")}")
 
-    // for every seq (as 4 lines), drop 3 lines
-    val dnaSeqs = recordsRdd.filter(drop3Records)
-    println("dnaSeqs : ", dnaSeqs.collect().mkString("Array(", ", ", ")"))
-
     // if you do not have enough RAM, then do the following
     // recordsRdd.persist(StorageLevel.MEMORY_AND_DISK)
-
-    val pairsRdd = dnaSeqs.mapPartitions(processFASTQPartition)
+    val pairsRdd = recordsRdd.mapPartitions(processFASTQPartition)
     println("pairsRdd : debug")
-    val pairsAsList = pairsRdd.collect()
-    println("pairsAsList : ", pairsAsList.mkString("Array(", ", ", ")"))
 
     val frequenciesRdd = pairsRdd.reduceByKey((x, y) => x + y)
     println("frequenciesRdd : debug")
@@ -102,8 +70,6 @@ object DNABaseCountFastq {
     println("frequenciesAsList : ", frequenciesAsList.mkString("Array(", ", ", ")"))
 
     spark.stop()
-
-
   }
 
 }
