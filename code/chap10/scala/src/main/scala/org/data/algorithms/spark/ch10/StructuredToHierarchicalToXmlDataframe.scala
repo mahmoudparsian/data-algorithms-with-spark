5f4ac78714a5bca872f2eaacfd82349eac63d051
Deepak <deepakmca05@gmail.com>
2022-01-27 18:07:08 +0530

    StructuredToHierarchicalToXmlRDD

--- code/chap10/scala/src/main/scala/org/data/algorithms/spark/ch10/StructuredToHierarchicalToXmlDataframe.scala
+++ code/chap10/scala/src/main/scala/org/data/algorithms/spark/ch10/StructuredToHierarchicalToXmlDataframe.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession
 /*-----------------------------------------------------
  * This is an example of  "Structured to Hierarchical" Pattern in PySpark.
  *
- * RRD-based solution
+ * Dataframe-based solution
  * ------------------------------------------------------
  * NOTE: print() and collect() are used for debugging and educational purposes.
  *------------------------------------------------------
@@ -15,7 +15,7 @@ import org.apache.spark.sql.SparkSession
  *-------------------------------------------------------
 
 */
-object StructuredToHierarchicalToXmlRDD {
+object StructuredToHierarchicalToXmlDataframe {
   /*
   -----------------------------------
    element: (post_id, Iterable<((title, creator),(comment, commented_by))>)
@@ -50,34 +50,47 @@ object StructuredToHierarchicalToXmlRDD {
     return xmlString
   }
 
+
   def main(args: Array[String]): Unit = {
     //create an instance of SparkSession object
     val spark = SparkSession.builder.master("local[*]").getOrCreate()
-    val postsData = List(("p1", ("t1", "creator1")), ("p2", ("t2", "creator2")), ("p3", ("t3", "creator3")))
+    val postsData = List(("p1", "t1", "creator1"), ("p2", "t2", "creator2"), ("p3", "t3", "creator3"))
     val commentsData = List(
-      ("p1", ("comment-11", "by-11")),
-      ("p1", ("comment-12", "by-12")),
-      ("p1", ("comment-13", "by-13")),
-      ("p2", ("comment-21", "by-21")),
-      ("p2", ("comment-22", "by-22")),
-      ("p2", ("comment-23", "by-23")),
-      ("p2", ("comment-24", "by-24")),
-      ("p3", ("comment-31", "by-31")),
-      ("p3", ("comment-32", "by-32"))
+      ("p1", "comment-11", "by-11"),
+      ("p1", "comment-12", "by-12"),
+      ("p1", "comment-13", "by-13"),
+      ("p2", "comment-21", "by-21"),
+      ("p2", "comment-22", "by-22"),
+      ("p2", "comment-23", "by-23"),
+      ("p2", "comment-24", "by-24"),
+      ("p3", "comment-31", "by-31"),
+      ("p3", "comment-32", "by-32")
     )
-    val posts = spark.sparkContext.parallelize(postsData)
+    //val postsSchema = List(StructField("post_id", StringType, true), StructField("title", StringType, true),StructField("title", StringType, true))
+    val postsColumns: Seq[String] = Seq("post_id","title","creator")
+    val posts = spark.createDataFrame(postsData).toDF(postsColumns:_*)
     println(s"posts.count() : ${posts.count()}")
-    println(s"posts.collect() : ${posts.collect().mkString("Array(", ", ", ")")}")
-    val comments = spark.sparkContext.parallelize(commentsData)
+    posts.show(truncate=false)
+
+    val commentColumns: Seq[String] = Seq("post_id","comment","commented_by")
+    val comments = spark.createDataFrame(commentsData).toDF(commentColumns:_*)
     println(s"posts.count() : ${posts.count()}")
-    println(s"posts.collect() : ${posts.collect().mkString("Array(", ", ", ")")}")
-    val joined = posts.join(comments)
-    val grouped = joined.groupByKey()
+    posts.show(truncate=false)
+
+    val joinedAndSelected = posts.join(comments , posts("post_id") === comments("post_id")).
+      select(posts("post_id"),posts("title"),posts("creator"),comments("comment"))
+    joinedAndSelected.show(truncate = false)
+
+    val grouped = joinedAndSelected.groupBy("post_id", "title", "creator").agg(functions.("comment").alias("comments"))
+
+    val createXMLUDF =
+    val df =
+    /*val grouped = joined.groupByKey()
     println(s"grouped.count() : ${grouped.count()}")
     println(s"grouped.collect() : ${grouped.mapValues(vs => vs.toList).collect().mkString("Array(", ", ", ")")}")
     val xmlRDD = grouped.map(createXML)
     println(s"xmlRDD.count() : ${xmlRDD.count()}")
-    println(s"xmlRDD.collect() : ${xmlRDD.collect().mkString("Array(", ", ", ")")}")
+    println(s"xmlRDD.collect() : ${xmlRDD.collect().mkString("Array(", ", ", ")")}")*/
 
   }
 }
