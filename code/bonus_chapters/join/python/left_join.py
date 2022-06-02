58621120d1fcb9e0e5b30d9a7013181adab09241
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2022-06-02 11:06:58 -0700

    added joins

--- code/bonus_chapters/join/python/left_join.py
+++ code/bonus_chapters/join/python/left_join.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 # create an instance of SparkSession
 spark = SparkSession.builder.getOrCreate()
 
-# find inner join(A, B)
+# find left join(R, S)
 
 # A record format "<key><,><value>"
 # A = spark.sparkContext.textFile("/tmp/A.txt");
@@ -23,15 +23,16 @@ rdd_B = B.map(lambda s: s.split(",")).flatMap(lambda s: [(s[0], s[1])])
 print("rdd_B=", rdd_B.collect())
 
 """
-pyspark.RDD.join
-RDD.join(other, numPartitions=None)
-Return an RDD containing all pairs of elements 
-with matching keys in self and other.
-Each pair of elements will be returned as a 
-(k, (v1, v2)) tuple, where (k, v1) is in self 
-and (k, v2) is in other.
-Performs a hash join across the cluster.
+RDD.leftOuterJoin(other, numPartitions=None)
+Perform a left outer join of self and other.
+For each element (k, v) in self, the resulting 
+RDD will either contain all pairs (k, (v, w)) 
+for w in other, or the pair (k, (v, None)) if 
+no elements in other have key k.
+Hash-partitions the resulting RDD into the given 
+number of partitions.
 """
+
 # A_joined_B: RDD[(String, (String, String))]
-A_joined_B = rdd_A.join(rdd_B)
-print("A_joined_B=", A_joined_B.collect())
+A_left_joined_B = rdd_A.leftOuterJoin(rdd_B)
+print("A_left_joined_B=", A_left_joined_B.collect())
--- code/bonus_chapters/join/python/right_join.py
+++ code/bonus_chapters/join/python/right_join.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 # create an instance of SparkSession
 spark = SparkSession.builder.getOrCreate()
 
-# find inner join(A, B)
+# find right join(A, B)
 
 # A record format "<key><,><value>"
 # A = spark.sparkContext.textFile("/tmp/A.txt");
@@ -23,15 +23,17 @@ rdd_B = B.map(lambda s: s.split(",")).flatMap(lambda s: [(s[0], s[1])])
 print("rdd_B=", rdd_B.collect())
 
 """
-pyspark.RDD.join
-RDD.join(other, numPartitions=None)
-Return an RDD containing all pairs of elements 
-with matching keys in self and other.
-Each pair of elements will be returned as a 
-(k, (v1, v2)) tuple, where (k, v1) is in self 
-and (k, v2) is in other.
-Performs a hash join across the cluster.
+pyspark.RDD.rightOuterJoin
+RDD.rightOuterJoin(other, numPartitions=None)
+Perform a right outer join of self and other.
+For each element (k, w) in other, the resulting 
+RDD will either contain all pairs (k, (v, w)) 
+for v in this, or the pair (k, (None, w)) if no 
+elements in self have key k.
+Hash-partitions the resulting RDD into the given 
+number of partitions.
+
 """
 # A_joined_B: RDD[(String, (String, String))]
-A_joined_B = rdd_A.join(rdd_B)
-print("A_joined_B=", A_joined_B.collect())
+A_right_joined_B = rdd_A.rightOuterJoin(rdd_B)
+print("A_right_joined_B=", A_right_joined_B.collect())
